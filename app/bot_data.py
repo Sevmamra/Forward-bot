d8d560c2bdccbb402a59977d2cd3f8842376a1bf
Sevmamra <sevmamra01@gmail.com>
2025-05-24 20:52:44 +0530

    Update bot_data.py

--- app/bot_data.py
+++ app/bot_data.py
@@ -6,8 +6,12 @@ class BotData:
         self.received_items = {
             'videos': 0,
             'files': 0,
-            'texts': 0
+            'texts': 0,
+            'photos': 0
         }
         self.collecting = False
         self.selected_groups = set()
         self.messages_to_forward = []
+
+# Create the shared instance
+bot_data = BotData()

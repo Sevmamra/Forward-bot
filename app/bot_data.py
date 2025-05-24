9117d4967751bdb3150463b59b194c7a8caa60f5
Sevmamra <sevmamra01@gmail.com>
2025-05-24 18:41:03 +0530

    Update and rename bot.data to bot_data.py

---
+++ app/bot_data.py
@@ -0,0 +1,13 @@
+class BotData:
+    def __init__(self):
+        self.reset()
+        
+    def reset(self):
+        self.received_items = {
+            'videos': 0,
+            'files': 0,
+            'texts': 0
+        }
+        self.collecting = False
+        self.selected_groups = set()
+        self.messages_to_forward = []

70253c91796675acaf14d70e4c6ad01951a1c678
Sevmamra <sevmamra01@gmail.com>
2025-05-24 20:54:20 +0530

    Update messages.py

--- app/handlers/messages.py
+++ app/handlers/messages.py
@@ -1,8 +1,7 @@
-from telegram.ext import MessageHandler, filters  # Corrected import
+from telegram.ext import MessageHandler, filters
 from app.config import Config
 from app.bot_data import bot_data
 
-# Rest of your messages.py code
 async def handle_message(update, context):
     if not bot_data.collecting or update.effective_user.id != Config.AUTHORIZED_USER_ID:
         return
@@ -12,8 +11,12 @@ async def handle_message(update, context):
         bot_data.received_items['videos'] += 1
     elif message.document:
         bot_data.received_items['files'] += 1
-    else:
+    elif message.photo:
+        bot_data.received_items['photos'] += 1
+    elif message.text:
         bot_data.received_items['texts'] += 1
+
+    bot_data.messages_to_forward.append(message)
 
 def setup_messages(application):
     application.add_handler(MessageHandler(

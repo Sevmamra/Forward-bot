3dc457569e73d6c79ef64a3e3467e8ef15d08052
Sevmamra <sevmamra01@gmail.com>
2025-05-24 18:47:45 +0530

    Update config.py

--- app/config.py
+++ app/config.py
@@ -1,6 +1,6 @@
 import os
 
 class Config:
-    TOKEN = os.getenv("TELEGRAM_BOT_TOKEN")
-    AUTHORIZED_USER_ID = int(os.getenv("AUTHORIZED_USER_ID", 0))
-    GROUP_IDS = [int(x) for x in os.getenv("GROUP_IDS", "").split(",") if x]
+    TOKEN = os.getenv("7763888885:AAGGtCSMRc0tvvGCFJwRMmYJBR819JfiOmQ")
+    AUTHORIZED_USER_ID = int(os.getenv("6567162029", 0))
+    GROUP_IDS = [int(x) for x in os.getenv("-1002501498159", "-1002665578655").split(",") if x]

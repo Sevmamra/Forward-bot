2dfb0a3c5f0cb5f516248812cccfb1201a0a102d
Sevmamra <sevmamra01@gmail.com>
2025-05-27 10:23:14 +0530

    Update telegram_utils.py

--- app/utils/telegram_utils.py
+++ app/utils/telegram_utils.py
@@ -1,11 +1,9 @@
 from telegram.error import TelegramError
-from telegram.constants import ParseMode
 import logging
 
 logger = logging.getLogger(__name__)
 
 async def fetch_topics(bot, chat_id):
-    """Fetch topics with error handling"""
     try:
         forum = await bot.get_forum_topics(chat_id)
         return {topic.message_thread_id: topic.name for topic in forum.topics}
@@ -14,7 +12,6 @@ async def fetch_topics(bot, chat_id):
         return {}
 
 async def create_topic(bot, chat_id, name):
-    """Create topic with error handling"""
     try:
         topic = await bot.create_forum_topic(
             chat_id=chat_id,
